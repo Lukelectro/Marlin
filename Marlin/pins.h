2a3cbe07266f1cd5447456f5cc1b935f2aaaf692
Victor Andueza <victor.andueza@bq.com>
2015-01-28 16:52:36 +0100

    Add bq Witbox 3D printer

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -2258,6 +2258,8 @@ Fan_2 2
   #include "pins_5DPRINT.h"
 #elif MB(LEAPFROG)
   #include "pins_LEAPFROG.h"
+#elif MB(WITBOX)
+  #include "pins_WITBOX.h"
 #elif MB(99)
   #include "pins_99.h"
 #else
