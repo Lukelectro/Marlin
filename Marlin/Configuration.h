3fabeaa377f3394c913de8317f91da6fb705f9fe
AnHardt <github@kitelab.de>
2015-03-15 16:44:37 +0100

    Removed Kanji.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -513,7 +513,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // JFR -was true // set to true to i
 //==============================LCD and SD support=============================
 
 // Define your display language below. Replace (en) with your language code and uncomment.
-// en, pl, fr, de, es, ru, it, pt, pt-br, fi, an, nl, ca, eu, kana, kana_utf8, kanji, test
+// en, pl, fr, de, es, ru, it, pt, pt-br, fi, an, nl, ca, eu, kana, kana_utf8, test
 // See also language.h
 #define LANGUAGE_INCLUDE GENERATE_LANGUAGE_INCLUDE(en)
 
