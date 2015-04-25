b471d62dffdc1215127aaeb898d5302a2ec03cfa
Scott Lahteine <sourcetree@thinkyhead.com>
2015-04-25 00:46:33 -0700

    Add a warning for DISABLE_[XYZ] options

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -367,6 +367,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // JFR -was true // set to true to i
 #define E_ENABLE_ON 0 // For all extruders
 
 // Disables axis when it's not being used.
+// WARNING: When motors turn off there is a chance of losing position accuracy!
 #define DISABLE_X false
 #define DISABLE_Y false
 #define DISABLE_Z false
