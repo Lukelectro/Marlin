02bb21b0bbfdada6883ad14640691b5b4e8b04ac
Richard Wackerbarth <rkw@dataplex.net>
2015-05-24 11:32:34 -0500

    Compiler defines USBCON for USB devices (PR#2433)

--- Marlin/stepper.cpp
+++ Marlin/stepper.cpp
@@ -650,7 +650,7 @@ ISR(TIMER1_COMPA_vect) {
 
     // Take multiple steps per interrupt (For high speed moves)
     for (int8_t i = 0; i < step_loops; i++) {
-      #ifndef AT90USB
+      #ifndef USBCON
         MSerial.checkRx(); // Check for serial chars.
       #endif
 
