c723d404873fdb07631a5ee77201159a05c062d7
Scott Lahteine <sourcetree@thinkyhead.com>
2015-05-28 17:44:09 -0700

    Ignore unknown commands if they start with G, M, or T

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -5380,7 +5380,7 @@ void process_next_command() {
         gcode_G92();
         break;
 
-      default: code_is_good = false;
+      // default: code_is_good = false;
     }
     break;
 
@@ -5470,8 +5470,6 @@ void process_next_command() {
       case 109: // M109: Wait for temperature
         gcode_M109();
         break;
-
-      case 110: break; // M110: Set line number - don't show "unknown command"
 
       #if HAS_TEMP_BED
         case 190: // M190: Wait for bed heater to reach target
@@ -5814,13 +5812,15 @@ void process_next_command() {
         gcode_M999();
         break;
 
-      default: code_is_good = false;
+      // default: code_is_good = false;
     }
     break;
 
     case 'T':
       gcode_T(codenum);
     break;
+
+    default: code_is_good = false;
   }
 
 ExitUnknownCommand:
