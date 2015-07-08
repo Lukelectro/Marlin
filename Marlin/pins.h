14d4f6f7ca72e77b363362db336667d98e9b2be4
Scott Lahteine <sourcetree@thinkyhead.com>
2015-07-08 15:30:33 -0700

    RigidBoard include for pins.h

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -2270,6 +2270,8 @@ Fan_2 2
   #include "pins_FELIX2.h"
 #elif MB(MKS_BASE)
   #include "pins_MKS_BASE.h"
+#elif MB(RIGIDBOARD)
+  #include "pins_RIGIDBOARD.h"
 #elif MB(99)
   #include "pins_99.h"
 #else
