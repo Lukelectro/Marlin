8f454089c64abaf2a56b087772f0e4fa3e61276b
Scott Lahteine <sourcetree@thinkyhead.com>
2016-04-02 18:53:05 -0700

    Move default machine info to pins files

--- Marlin/language.h
+++ Marlin/language.h
@@ -80,22 +80,14 @@
   #define DEFAULT_MACHINE_NAME "3D Printer"
 #endif
 
-#if MB(ULTIMAKER)|| MB(ULTIMAKER_OLD)|| MB(ULTIMAIN_2)
-  #define DEFAULT_SOURCE_URL "https://github.com/Ultimaker/Marlin"
-#elif MB(3DRAG)
-  #define DEFAULT_SOURCE_URL "http://3dprint.elettronicain.it/"
-#elif MB(K8200)
-  #define DEFAULT_SOURCE_URL "https://github.com/CONSULitAS/Marlin-K8200"
-#elif MB(SAV_MKI)
-  #define DEFAULT_SOURCE_URL "https://github.com/fmalpartida/Marlin/tree/SAV-MkI-config"
-#else
-  #define DEFAULT_SOURCE_URL "https://github.com/MarlinFirmware/Marlin"
-#endif
-
 #ifdef CUSTOM_MACHINE_NAME
   #define MACHINE_NAME CUSTOM_MACHINE_NAME
 #else
   #define MACHINE_NAME DEFAULT_MACHINE_NAME
+#endif
+
+#ifndef DEFAULT_SOURCE_URL
+  #define DEFAULT_SOURCE_URL "https://github.com/MarlinFirmware/Marlin"
 #endif
 
 #ifndef SOURCE_CODE_URL
