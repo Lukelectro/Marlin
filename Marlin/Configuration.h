a4b99453e1111bf608aa25ca80d1840ade2b74a5
gralco <kuzmenko@alephobjects.com>
2016-04-12 08:40:57 -0600

    Implementation of M190 bed temp hysteresis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -202,6 +202,10 @@
 #define TEMP_HYSTERESIS 3       // (degC) range of +/- temperatures considered "close" to the target one
 #define TEMP_WINDOW     1       // (degC) Window around target to start the residency timer x degC early.
 
+#define TEMP_BED_RESIDENCY_TIME 10  // (seconds)
+#define TEMP_BED_HYSTERESIS 3       // (degC) range of +/- temperatures considered "close" to the target one
+#define TEMP_BED_WINDOW     1       // (degC) Window around target to start the residency timer x degC early.
+
 // The minimal temperature defines the temperature below which the heater will not be enabled It is used
 // to check that the wiring to the thermistor is not broken.
 // Otherwise this would lead to the heater being powered on all the time.
