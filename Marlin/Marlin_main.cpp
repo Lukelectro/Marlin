23b6d269717d0fe18029d5b1b3b8a5a7b7aa2e67
gralco <kuzmenko@alephobjects.com>
2016-04-25 08:33:33 -0600

    Introduce M108 cancel heatup for the hotend and bed
    This G-code is asynchronously handled in the get_serial_commands() parser.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -963,6 +963,7 @@ inline void get_serial_commands() {
 
       // If command was e-stop process now
       if (strcmp(command, "M112") == 0) kill(PSTR(MSG_KILLED));
+      if (strcmp(command, "M108") == 0) cancel_heatup = true;
 
       #if defined(NO_TIMEOUTS) && NO_TIMEOUTS > 0
         last_command_time = ms;
@@ -4259,6 +4260,13 @@ inline void gcode_M104() {
 #endif // FAN_COUNT > 0
 
 /**
+ * M108: Cancel heatup and wait for the hotend and bed, this G-code is asynchronously handled in the get_serial_commands() parser
+ */
+inline void gcode_M108() {
+  cancel_heatup = true;
+}
+
+/**
  * M109: Sxxx Wait for extruder(s) to reach temperature. Waits only when heating.
  *       Rxxx Wait for extruder(s) to reach temperature. Waits when heating and cooling.
  */
@@ -6519,6 +6527,10 @@ void process_next_command() {
         gcode_M105();
         KEEPALIVE_STATE(NOT_BUSY);
         return; // "ok" already printed
+
+      case 108:
+        gcode_M108();
+        break;
 
       case 109: // M109: Wait for temperature
         gcode_M109();
