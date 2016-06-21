00f72d05c520142919bf35b7c8c036006f1b6d6d
brupje <brupje@gmail.com>
2016-06-21 14:39:10 +0200

    Support for REPRAPWORLD_GRAPHICAL_LCD

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -961,6 +961,13 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // JFR -was true // set to true to i
 //#define MAKRPANEL
 
 //
+// ReprapWorld Graphical LCD
+// https://reprapworld.com/?products_details&products_id/1218
+//
+//#define REPRAPWORLD_GRAPHICAL_LCD
+
+
+//
 // Activate one of these if you have a Panucatt Devices
 // Viki 2.0 or mini Viki with Graphic LCD
 // http://panucatt.com
