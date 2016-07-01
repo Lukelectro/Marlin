2689e18441b7e2738f3219b413edd1e12fe7ef83
AnHardt <github@kitelab.de>
2016-07-01 15:28:41 +0200

    Unconditional do_probe_raise() in probe_pt()
    If Z_RAISE_BETWEEN_PROBINGS is null or undefined the probe is currently not raised by home_offset[Z_AXIS] and zprobe_zoffset.
    But when different from 0 is.
    
    If an undefined Z_RAISE_BETWEEN_PROBINGS expands to 0 (and it does) this is the solution.
    
    A similar asymmetry exists with the newly introduced 'short-cut' in G28 - but its the rise before anything is probed - so should not make a difference.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -2058,14 +2058,12 @@ static void retract_z_probe() {
       #endif
       stow_z_probe();
     }
-    #if Z_RAISE_BETWEEN_PROBINGS > 0
-      else {
-        #if ENABLED(DEBUG_LEVELING_FEATURE)
-          if (DEBUGGING(LEVELING)) SERIAL_ECHOLNPGM("> do_probe_raise");
-        #endif
-        do_probe_raise(Z_RAISE_BETWEEN_PROBINGS);
-      }
-    #endif
+    else {
+      #if ENABLED(DEBUG_LEVELING_FEATURE)
+        if (DEBUGGING(LEVELING)) SERIAL_ECHOLNPGM("> do_probe_raise");
+      #endif
+      do_probe_raise(Z_RAISE_BETWEEN_PROBINGS);
+    }
 
     if (verbose_level > 2) {
       SERIAL_PROTOCOLPGM("Bed X: ");
