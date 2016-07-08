b86b181bbd798d0a5319b8c67f95cf4d5b59aab0
AnHardt <github@kitelab.de>
2016-07-08 13:47:40 +0200

    Remove HAS_BED_PROBE case in first raise in G28
    Differentiating made sense here when we could save a dozen lines of code,
    but not for one line.

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -2786,14 +2786,8 @@ inline void gcode_G28() {
           #endif
 
           feedrate = homing_feedrate[Z_AXIS];
-
-          #if HAS_BED_PROBE
-            do_blocking_move_to_z(z_dest);
-          #else
-            line_to_z(z_dest);
-            stepper.synchronize();
-          #endif
-
+          line_to_z(z_dest);
+          stepper.synchronize();
           destination[Z_AXIS] = current_position[Z_AXIS] = z_dest;
         }
       }
